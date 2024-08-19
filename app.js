6ec69c5c22a09506c52d0a072fca9effd01f699a
kitamura0102 <raul_almonte001@hotmail.com>
2024-08-18 22:58:41 -0400

    almost done

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-// Initiate a canvas instance
-var canvas = new fabric.Canvas("canvas");
-var circle = new fabric.Circle({
-   left: 115,
-   top: 100,
-   fill: "white",
-   radius: 50,
-   stroke: "black",
-   strokeWidth: 5,
-   hoverCursor: "text"
-});
-
-// Adding it to the canvas
-canvas.add(circle);
-canvas.setWidth(document.body.scrollWidth);
-canvas.setHeight(250);
+const letter = "Raul Almonte"
+const nothing = [];
+const typing = document.getElementById("typing")
