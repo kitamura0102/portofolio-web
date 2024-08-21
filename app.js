9aed7f34a2d0f4b7a4fdfe2f7bc3304bef8f1cba
kitamura0102 <raul_almonte001@hotmail.com>
2024-08-20 23:47:18 -0400

    Added a scroll animation, completed all the mediaQuery, Added just the hamburget btn. will stylit it later

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-const letter = "Raul Almonte"
-const nothing = [];
-const typing = document.getElementById("typing")
+const sections = document.querySelectorAll('.hidden');
+
+window.addEventListener('scroll', function() {
+    sections.forEach(section => {
+        const position = section.getBoundingClientRect();
+
+        // Si la parte superior del elemento está dentro de la ventana del navegador
+        if (position.top < window.innerHeight && position.bottom >= 0) {
+            section.style.opacity = 1; // Elemento visible
+        } else {
+            section.style.opacity = 0; // Elemento invisible
+        }
+    });
+});
